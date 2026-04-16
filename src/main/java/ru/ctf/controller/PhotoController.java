b8adb9a91d85d31ae1dd533cfa29af87167ad138
root <root@lxc-8b87dea7-aac4-4d2b-8a8c-c07e404ded74.incus>
2026-04-16 05:12:48 +0000

    Fixed

--- src/main/java/ru/ctf/controller/PhotoController.java
+++ src/main/java/ru/ctf/controller/PhotoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.ServletContextAware;
 
 import java.io.*;
+import java.nio.file.Path;
 import java.security.InvalidAlgorithmParameterException;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -50,12 +51,24 @@ public class PhotoController implements ServletContextAware {
     public void getPhoto(@RequestParam(name = "file") String fileName, HttpServletResponse httpServletResponse, ServletRequest servletRequest) throws InvalidAlgorithmParameterException {
         logger.info("URI: {} host: {} file:{}", ((HttpServletRequest)servletRequest).getRequestURI(), servletRequest.getRemoteHost(), fileName);
 
-        String fullPath = pathDirPhoto + '/' + fileName;
-        File downloadFile = new File(fullPath);
-        try (InputStream is = new FileInputStream(new File(fullPath));
+        Path basePath = new File(pathDirPhoto).toPath().toAbsolutePath().normalize();
+        Path requestedPath = basePath.resolve(fileName).normalize();
+
+        if (!requestedPath.startsWith(basePath)) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        File downloadFile = requestedPath.toFile();
+        if (!downloadFile.isFile()) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        try (InputStream is = new FileInputStream(downloadFile);
              OutputStream outStream = httpServletResponse.getOutputStream();
         ) {
-            String mimeType = servletContext.getMimeType(fullPath);
+            String mimeType = servletContext.getMimeType(downloadFile.getAbsolutePath());
             if (mimeType == null) {
                 mimeType = "application/octet-stream";
             }
